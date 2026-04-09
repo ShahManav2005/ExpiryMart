e6c6434eeaf1bf408863f86addf2ec03449484b6
ShahManav2005 <manavshah5577@gmail.com>
2026-04-09 22:27:32 +0530

    backend ready for deployment

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from routes.seller_routes import seller_routes
@@ -29,4 +30,5 @@ def home():
     return {"message": "ExpiryMart Backend Running"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
